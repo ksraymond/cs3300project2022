ab8fc375d08893ad3e9c4e9c034fb815b132bb4d
Keaton Raymond <ksraymond00@gmail.com>
2022-11-21 20:25:42 +0000

    Final iteration 4 commit with unknown testing errors resulting in a control frame dump and the message aborted when trying to run any tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,28 @@
-require 'rails_helper'
+# require 'rails_helper'
+# RSpec.describe Project, type: :model do
+#   it "has a valid description" do
+#     expect(Project.description).to be_valid
+#   end
+# end
+
+require "rails_helper"
+
 RSpec.describe Project, type: :model do
-  it "has a valid description" do
-    expect(Project.description).to be_valid
+  context "scopes tests" do
+    let(:params) { { title: "Title", description: "some description" } }
+    before(:each) do
+      Project.create(params)
+      Project.create(params)
+      Project.create(params)
+    end
+
+    # it "has a valid description" do
+    #   expect(Project.description).to be_valid
+    # end
+
+    it "should return all projects" do
+      expect(Project.count).to eq(3)
+    end
+      
   end
 end
